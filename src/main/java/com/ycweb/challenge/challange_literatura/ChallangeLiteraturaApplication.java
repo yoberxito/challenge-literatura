06b68e06d062e8d01307ab2e84119054f7f99df4
yober cieza <gctic.sgsass12@essalud.gob.pe>
2025-08-06 11:58:38 -0500

    Version inicial

---
+++ src/main/java/com/ycweb/challenge/challange_literatura/ChallangeLiteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.ycweb.challenge.challange_literatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallangeLiteraturaApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/com/ycweb/challenge/challange_literatura/ChallangeLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycweb.challenge.challange_literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallangeLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
